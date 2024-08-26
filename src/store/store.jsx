de7975f85c90f8a37bb2f0f17491a64392d7a83d
Talha-TN <nawazt964@gmail.com>
2024-08-26 11:36:19 -0700

    dleted previous redux code

--- src/store/store.jsx
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import counterslice from "./slices/counterslice";
-export const store = configureStore({
-    reducer:{
-        counter:counterslice,
-    },
-})
