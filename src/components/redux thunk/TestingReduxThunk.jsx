ccd7c6383f82789e8c86893d8204d2c9e9596900
Talha-TN <nawazt964@gmail.com>
2024-11-28 10:53:54 -0800

    removed unnessasry comments

--- src/components/redux thunk/TestingReduxThunk.jsx
+++ src/components/redux thunk/TestingReduxThunk.jsx
@@ -1,28 +1,4 @@
-// import React, { useRef, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { fetchTodo } from "./ToDoSlice";
-// import { add } from "../reduxToolkit/ReduxCounterSlice";
-// const TestingReduxThunk = () => {
-//     const [name,setName]=useState("")
 
-//   const dispatch = useDispatch();
-//   const state = useSelector((state) => state.todoSlice);
-//  const handleAdd=()=>{
-//     dispatch(add(name))
-//  }
-//   return (
-//     <div>
-//       {state.data &&
-//         state.data.map((el, index) => <p key={index}>{el.title}</p>)}
-//       <input onChange={(e)=>setName(e.target.value)} type="text" placeholder="enter name" />
-//       <button onClick={(e) => dispatch(fetchTodo())}>fetch task</button>
-//       <button onClick={handleAdd}>add</button>
-//     </div>
-//   );
-// };
-
-// export default TestingReduxThunk;
-////////////////////////////////
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchTodo, add } from "./ToDoSlice"; // Correct import
--- src/components/redux thunk/ToDoSlice.jsx
+++ src/components/redux thunk/ToDoSlice.jsx
@@ -1,39 +1,4 @@
-// import { createSlice,createAsyncThunk } from "@reduxjs/toolkit";
-// export const fetchTodo =createAsyncThunk("fetchtodo,",async ()=>{ // fetch to do action
-//      let response = await fetch("https://jsonplaceholder.typicode.com/todos")
-//      response= await  response.json()
-//      return response
-     
-// })
-// const todoSlice = createSlice({
-//     name :"todoSlice",
-//     initialState:{
-//         isLoading:false,
-//         data:null,
-//         isError:false
-//     },
-//     reducers:{
-//         add:(state,action)=>{
-//             state.data.push(action.payload)
-//         }
-//     },
-    
-//     extraReducers:(builder)=>{ // for listen to changes in fetchTodo action
-//     builder.addCase(fetchTodo.pending,(state,action)=>{
-//         state.isLoading=true
-//     }),
-//     builder.addCase(fetchTodo.fulfilled,(state,action)=>{
-//         state.isLoading =true
-//         state.data=action.payload
-//     }),
-//     builder.addCase(fetchTodo.rejected,(state,action)=>{
-//         console.log("error",action.payload)
-//         state.isError=true
-//     })
-//     }
-// })
-// const {add} = todoSlice.actions
-// export default todoSlice.reducer
+
 ////////////////////////////////////////////////
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 export const fetchTodo = createAsyncThunk("fetchtodo", async () => {
